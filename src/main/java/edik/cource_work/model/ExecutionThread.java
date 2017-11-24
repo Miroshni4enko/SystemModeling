aa9f981efb029c5136cfbc4e777137c0a6d13f27
vymi1016 <vyacheslav.miroshnichenko@netcracker.com>
2017-11-24 15:26:12 +0200

    add arrive timer

--- src/main/java/edik/cource_work/model/ExecutionThread.java
+++ src/main/java/edik/cource_work/model/ExecutionThread.java
@@ -2,7 +2,8 @@ package edik.cource_work.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
@@ -15,6 +16,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ExecutionThread implements Callable<List<Integer>> {
     private AtomicInteger amountOfTasks = new AtomicInteger();
     private AtomicBoolean stop = new AtomicBoolean();
+    private AtomicInteger executionQueue = new AtomicInteger(0);
     private final int amountOfNeededPermit;
     private Semaphore semaphore;
     private TaskType taskType;
@@ -31,27 +33,25 @@ public class ExecutionThread implements Callable<List<Integer>> {
     }
 
     public List<Integer> call() throws InterruptedException {
-
         List<Integer> resultList = new ArrayList<Integer>();
-            while (!stop.get()) {
-                System.out.println(stop.get() + taskType.name());
-                if (amountOfTasks.get() == 0) {
-                    Thread.currentThread().yield();
-                } else {
-                    semaphore.acquire(amountOfNeededPermit);
-                    executeTasks(resultList);
-                    semaphore.release(amountOfNeededPermit);
-                }
+        startArriveTimer();
+        while (!stop.get()) {
+            System.out.println(stop.get() + taskType.name());
+            if (executionQueue.get() == 0) {
+                Thread.currentThread().yield();
+            } else {
+                semaphore.acquire(amountOfNeededPermit);
+                executeTasks(resultList);
+                semaphore.release(amountOfNeededPermit);
             }
-
-            return resultList;
-
+        }
+        return resultList;
     }
 
     private void executeTasks(List<Integer> resultList) throws InterruptedException {
-        for ( ; amountOfTasks.get() > 0; amountOfTasks.decrementAndGet()){
-            int timeOfTask = taskType.getTime();
-            System.out.println("i = " + amountOfTasks +" taskType = "+ taskType.name() + "timeOfTask = " + timeOfTask);
+        for ( ; executionQueue.get() > 0; executionQueue.decrementAndGet()){
+            int timeOfTask = taskType.getTaskTime();
+            System.out.println("i = " + executionQueue +" taskType = "+ taskType.name() + "timeOfTask = " + timeOfTask);
             TimeUnit.SECONDS.sleep(timeOfTask);
             resultList.add(timeOfTask);
         }
@@ -60,5 +60,31 @@ public class ExecutionThread implements Callable<List<Integer>> {
 
     public void stop() {
         this.stop.set(true);
+    }
+
+    private void startArriveTimer(){
+        Thread thread = new Thread(new ArriveTaskTimer());
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    private class ArriveTaskTimer extends TimerTask {
+        public void run() {
+            try {
+                while (!stop.get()) {
+                    if (amountOfTasks.get() == 0) {
+                        Thread.currentThread().yield();
+                    } else {
+                        int timeArrive = taskType.getArriveTime();
+                        System.out.println("i = " + amountOfTasks +" taskType = "+ taskType.name() + "timeArrive = " + timeArrive);
+                        TimeUnit.SECONDS.sleep(timeArrive);
+                        amountOfTasks.decrementAndGet();
+                        executionQueue.incrementAndGet();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
