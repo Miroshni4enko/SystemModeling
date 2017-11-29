1d3db7e00d6066362b947de361611e8b58042111
vymi1016 <vyacheslav.miroshnichenko@netcracker.com>
2017-11-29 18:31:47 +0200

    first working demo

--- src/main/java/sumdu/cource_work/controller/ExecutionThreadsService.java
+++ src/main/java/sumdu/cource_work/controller/ExecutionThreadsService.java
@@ -3,6 +3,7 @@ package sumdu.cource_work.controller;
 import sumdu.cource_work.model.ExecutionThread;
 import sumdu.cource_work.model.InitThreads;
 import sumdu.cource_work.model.TaskType;
+import sumdu.cource_work.view.MainSchemaWindow;
 
 import java.util.*;
 import java.util.concurrent.*;
@@ -13,27 +14,27 @@ import static sumdu.cource_work.model.TaskType.*;
  * Created by Слава on 18.11.2017.
  */
 public class ExecutionThreadsService implements ExecutionService {
-    public final static int AMOUNT_PERMIT_FOR_PARALLEL_THREAD = 1;
-    public final static int AMOUNT_PERMIT_FOR_THREAD_WITH_CONDITION = 2;
-    private ExecutorService executorService = Executors.newFixedThreadPool(3);
+    public final static int AMOUNT_PERMIT_FOR_PARALLEL_THREAD = 0; // for A and B we  need no permits
+    public final static int AMOUNT_PERMIT_FOR_THREAD_WITH_CONDITION = 2;// when A and B tasks do release permit C get 2 permit
+    private ExecutorService executorService = Executors.newFixedThreadPool(6);
     private InitThreads initThreads;
     private List<Future<List<Integer>>> list;
 
-    public void startService(int amountOfTaskA, int amountOfTaskB, int amountOfTaskC) throws InterruptedException {
-        initThreads = new InitThreads(amountOfTaskA, amountOfTaskB, amountOfTaskC);
+    public void startService(int amountOfTaskA, int amountOfTaskB, int amountOfTaskC, MainSchemaWindow mainSchemaWindow) throws InterruptedException {
+        initThreads = new InitThreads(amountOfTaskA, amountOfTaskB, amountOfTaskC, mainSchemaWindow);
         Map<TaskType, ExecutionThread>  threadsMap =  initThreads.createExecutionThreads();
-        list = executorService.invokeAll(Arrays.asList(threadsMap.get(C),
-                                                         threadsMap.get(A),
-                                                         threadsMap.get(B)));
+        list = executorService.invokeAll(Arrays.asList(threadsMap.get(A),
+                                                       threadsMap.get(B),
+                                                       threadsMap.get(C)));
     }
 
     public Map<TaskType, List<Integer>> stopServiceAndGetResult()  {
         Map<TaskType, List<Integer>> resultMap = new EnumMap<TaskType, List<Integer>>(TaskType.class);
         initThreads.stopAllThreads();
         try {
-            resultMap.put(B , list.get(2).get());
-            resultMap.put(A , list.get(1).get());
-            resultMap.put(C , list.get(0).get());
+            resultMap.put(A , list.get(0).get());
+            resultMap.put(B , list.get(1).get());
+            resultMap.put(C , list.get(2).get());
             executorService.shutdown();
         } catch (InterruptedException e) {
             e.printStackTrace();
